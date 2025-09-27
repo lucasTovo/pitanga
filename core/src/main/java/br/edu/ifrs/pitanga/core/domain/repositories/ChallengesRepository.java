77206803a4c8a37b4343f561823f8de20d5423f0
Tovo <42346794+lucasTovo@users.noreply.github.com>
2025-09-27 18:45:02 -0300

    Adds filtering by creatorId when searching for challenges and adjusts return types in the listChallenges function

--- core/src/main/java/br/edu/ifrs/pitanga/core/domain/repositories/ChallengesRepository.java
+++ core/src/main/java/br/edu/ifrs/pitanga/core/domain/repositories/ChallengesRepository.java
@@ -3,6 +3,8 @@ package br.edu.ifrs.pitanga.core.domain.repositories;
 import java.util.UUID;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -14,4 +16,6 @@ public interface ChallengesRepository extends
     PagingAndSortingRepository<Challenge, UUID>,
     CrudRepository<Challenge, UUID>,
     JpaSpecificationExecutor<Challenge> {
+
+    Page<Challenge> findByCreatorId(String creatorId, Pageable pageable);
 }
