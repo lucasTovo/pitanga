5d8617fc61600f45ed95399425cb77cf987e0da5
Tovo <42346794+lucasTovo@users.noreply.github.com>
2025-09-28 07:13:44 -0300

    feat: update dependencies for use shadcn

--- pitangaweb/vite.config.ts
+++ pitangaweb/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
+import path from "path"
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
   },
   base: '/pitanga',
   envPrefix: 'PITANGA_',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
