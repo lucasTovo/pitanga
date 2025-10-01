d4df8fe16b37d3f928aa2990c9a21a6045215407
Tovo <42346794+lucasTovo@users.noreply.github.com>
2025-10-01 01:21:43 -0300

    feat: create or update keycloack logged-in user on school service

--- pitangaweb/src/main.tsx
+++ pitangaweb/src/main.tsx
@@ -1,13 +1,22 @@
 import ReactDOM from "react-dom/client";
 import { ReactKeycloakProvider } from "@react-keycloak/web";
-import { App } from "./App";
-import { keycloak, initOptions } from "./infra/data/keycloack";
+
 import "./main.css";
+
+import { App } from "./App";
+import { updateUser } from "./infra/data/shcool.rest";
+import { keycloak, initOptions } from "./infra/data/keycloack";
 
 const root = document.getElementById("root")!;
 
+const onAuthSuccess = (event: string) => {
+  if (event === 'onAuthSuccess') {
+    updateUser();
+  }
+}
+
 ReactDOM.createRoot(root).render(
-  <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
+  <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions} onEvent={onAuthSuccess}>
     <App />
   </ReactKeycloakProvider>
 );
