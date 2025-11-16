db71a1b24fa7a9e4fbea9925e7c937725f9b1c70
Tovo <42346794+lucasTovo@users.noreply.github.com>
2025-11-16 18:50:52 -0300

    refactor: remove unused components

--- pitangaweb/src/app/components/tool-tray/Title.tsx
+++
@@ -1,7 +0,0 @@
-export const Title = (props: { title: string; onClick?: () => void; }) => (
-  <h2
-    onClick={props.onClick}
-    className='text-xl max-h-7 font-bold text-ellipsis overflow-hidden text-nowrap'>
-    {props.title}
-  </h2>
-);
--- pitangaweb/src/app/components/tooltip/index.tsx
+++
@@ -1,8 +0,0 @@
-import './style.css';
-
-export const Tooltip = ({message}: { message: string; }) => (
-  <span className='tooltip'>
-    <img src='/pitanga-tcc/question.svg' className='h-4 w-4'/>
-    <span className='tooltiptext'>{message}</span>
-  </span>
-);
